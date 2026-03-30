c3a83b57e72466553dbf2b43a309f6ca215ada9a
xiebing <xiebing@fairlandgroup.com>
2026-03-30 14:10:02 +0800

    Update MAVLink XML hashes and adjust .gitignore for venv

--- generated/c/minimal/minimal.h
+++ generated/c/minimal/minimal.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_MINIMAL.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_MINIMAL_XML_HASH 1420910747539382900
+#define MAVLINK_MINIMAL_XML_HASH 4219931964808732066
 
 #ifdef __cplusplus
 extern "C" {
